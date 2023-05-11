799fa39e029970a3ccea6062a77ded27f9a6ebd1
lacykaltgr <freundl0509@gmail.com>
2023-05-11 23:51:48 +0200

    MAIN

--- classifier.py
+++ classifier.py
@@ -4,7 +4,7 @@ from keras.layers import Conv2D, MaxPool2D, Dropout, BatchNormalization, GroupNo
 
 class Encoder(keras.Sequential):
     def __init__(self, KERNEL_SIZE = (3, 3), INPUT_SHAPE = (32, 32, 3)):
-        super().__init__(
+        super().__init__([
             Conv2D(filters=32, kernel_size=KERNEL_SIZE, input_shape=INPUT_SHAPE, activation='relu', padding='same'),
             BatchNormalization(),
             Conv2D(filters=32, kernel_size=KERNEL_SIZE, input_shape=INPUT_SHAPE, activation='relu', padding='same'),
@@ -31,18 +31,19 @@ class Encoder(keras.Sequential):
             Conv2D(16, 3, strides=(1, 1)),
             ZeroPadding2D((1, 1)),
             Conv2D(8, 1, strides=(1, 1))
-        )
+        ])
 
 
 
 
 class Classifier(keras.Sequential):
     def __init__(self):
-        super().__init__(
+        super().__init__([
             Flatten(),
             Dropout(0.2),
             Dense(128, activation='relu'),
             Dropout(0.25),
             Dense(10, activation='softmax')
+        ]
         )
 
